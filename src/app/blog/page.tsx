b601a8945a0d1096096c9af8247d7036a8658553
Swann HERRERA <swann@devloup.dev>
2023-02-25 22:35:16 +0100

    update architecture

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,20 @@
-import { Menu } from "@/components/menu";
+import { Menu } from "@/components/organisms/menu";
+import { atom, useAtom } from 'jotai'
+import { atomsWithQuery } from 'jotai-tanstack-query'
+
+const idAtom = atom(1)
+const [userAtom] = atomsWithQuery((get) => ({
+  queryKey: ['users', get(idAtom)],
+  queryFn: async ({ queryKey: [, id] }) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    return res.json()
+  },
+}))
+
+const UserData = () => {
+  const [data] = useAtom(userAtom)
+  return <div>{JSON.stringify(data)}</div>
+}
 
 export default function Blog() {
   return (
