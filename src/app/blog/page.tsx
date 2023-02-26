c2ec588b56dcd1a715deb0cb83a05f4825182051
Swann HERRERA <swann@devloup.dev>
2023-02-27 00:48:13 +0100

    disable action and remove jotai code

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,21 +2,21 @@ import { Menu } from "@/components/organisms/menu";
 import { atom, useAtom } from 'jotai'
 import { atomsWithQuery } from 'jotai-tanstack-query'
 
-const idAtom = atom(1)
-const [userAtom] = atomsWithQuery((get) => ({
-  queryKey: ['users', get(idAtom)],
-  queryFn: async ({ queryKey: [, id] }) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-    return res.json()
-  },
-}))
+// const idAtom = atom(1)
+// const [userAtom] = atomsWithQuery((get) => ({
+//   queryKey: ['users', get(idAtom)],
+//   queryFn: async ({ queryKey: [, id] }) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+//     return res.json()
+//   },
+// }))
+//
+// const UserData = () => {
+//   const [data] = useAtom(userAtom)
+//   return <div>{JSON.stringify(data)}</div>
+// }
 
-const UserData = () => {
-  const [data] = useAtom(userAtom)
-  return <div>{JSON.stringify(data)}</div>
-}
-
-export default function Blog() {
+export default async function Blog() {
   return (
     <>
       <Menu />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Menu } from "@/components/menu";
+import { Menu } from "@/components/organisms/menu";
 
 export default function Home() {
   return (
